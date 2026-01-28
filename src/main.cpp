5147c4a2431f0f88d7e9f646cbc15d7a552549e2
qingzhixing <qzsqqmail@qq.com>
2026-01-28 09:06:47 +0800

    📦 NEW: 遍历/proc/目录，获取pid信息

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,12 @@
 #include <arguments_handler.h>
 #include <project_info.h>
 
+#include <fstream>
+#include <filesystem>
+#include <algorithm>
+
+namespace fs = std::filesystem;
+
 int main(int argc, char *argv[])
 {
     auto pstree_options = ArgumentsHandler::HandleArguments(argc, argv);
@@ -25,5 +31,26 @@ int main(int argc, char *argv[])
     {
         std::cout << "Numeric sort is enabled.\n";
     }
+
+    // 遍历 /proc/[pid]
+    auto proc_path = fs::path("/proc/");
+    if (!fs::exists(proc_path) || !fs::is_directory(proc_path))
+    {
+        std::cerr << "✖️ 无法打开 /proc/ 目录\n";
+        return 1;
+    }
+
+    for (const auto &entry : fs::directory_iterator(proc_path))
+    {
+        if (fs::is_directory(entry.status()))
+        {
+            std::string pid = entry.path().filename().string();
+            if (std::all_of(pid.begin(), pid.end(), ::isdigit))
+            {
+                std::cout << "Find PID: " << pid << std::endl;
+            }
+        }
+    }
+
     return 0;
 }
