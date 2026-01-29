4d14f0aa433eda22aec1e8f1bcd5f48e9c3358a5
qingzhixing <qzsqqmail@qq.com>
2026-01-30 00:20:30 +0800

    👌 IMPROVE: 更美观地打印！

--- include/pstree_display.h
+++ include/pstree_display.h
@@ -8,6 +8,7 @@
 #include <map>
 #include <iostream>
 #include <format>
+#include <testkit.h>
 
 struct TreeNodeInfo
 {
@@ -26,6 +27,7 @@ public:
 	void DisplayTree(const PstreeOptions &pstree_options)
 	{
 		// 前序遍历
+		// 第一个进程的 pid 一定是 1
 		PreorderTraversal(1, pstree_options);
 	}
 
@@ -33,10 +35,28 @@ private:
 	void PreorderTraversal(int pid, const PstreeOptions &pstree_options)
 	{
 		// 记录当前节点的深度，以便打印出树的形状
-		static int depth = 0;
+
+		// 用于管理打印时的连接线
+		static std::vector<bool> is_connector_required = {};
+
+		// 连接线前缀
+		if (!is_connector_required.empty())
+		{
+			for (int i = 0; i < is_connector_required.size() - 1; i++)
+			{
+				if (is_connector_required[i])
+				{
+					std::cout << " |  ";
+				}
+				else
+				{
+					std::cout << "    ";
+				}
+			}
+			std::cout << " +--";
+		}
 
 		// 打印当前节点的信息
-		std::cout << std::string(depth * 2, ' ');
 		std::cout << tree_nodes[pid].process_name;
 		if (pstree_options.show_pids)
 		{
@@ -50,11 +70,32 @@ private:
 		}
 
 		// 递归遍历子节点
-		for (int child_pid : node_children[pid])
+		for (int i = 0; i < node_children[pid].size(); i++)
 		{
-			depth++;
+			int child_pid = node_children[pid][i];
+
+			// 打印单独一行的连接线
+			if (!is_connector_required.empty())
+			{
+				for (int i = 0; i < is_connector_required.size(); i++)
+				{
+					if (is_connector_required[i])
+					{
+						std::cout << " |  ";
+					}
+					else
+					{
+						std::cout << "    ";
+					}
+				}
+			}
+			std::cout << " |" << std::endl;
+
+			bool is_last_child = (i == node_children[pid].size() - 1);
+
+			is_connector_required.push_back(!is_last_child);
 			PreorderTraversal(child_pid, pstree_options);
-			depth--;
+			is_connector_required.pop_back();
 		}
 	}
 
